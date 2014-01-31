7b0513087e92942fa774e9109e4122a600e96d52
noex1987 <samuel.verron@ingesup.com>
2014-01-31 22:27:37 +0100

    Ajout du service pour accès aux events.

--- Agenda/Model/Events.cs
+++ Agenda/Model/Events.cs
@@ -11,15 +11,15 @@ using System.Threading.Tasks;
 
 namespace Agenda.Model
 {
-    class Events
+    public class Events
     {
         public string name { get; set; }
         public int id { get; set; }
         public string description { get; set; }
-        private DateTime pointBegin;
-        private DateTime pointEnd;
-        private Periodicity period;
-        private List<Users> lesParticipants;
+        private DateTime pointBegin { get; set; }
+        private DateTime pointEnd { get; set; }
+        private int period { get; set; }
+        private List<Users> lesParticipants { get; set; }
         public string type { get; set; }
 
         public Events()
@@ -30,9 +30,20 @@ namespace Agenda.Model
             this.description = "";
             this.pointBegin = new DateTime();
             this.pointEnd = new DateTime();
-            this.period = null;
+            this.period = 0;
         }
-        public Events(string _name, string _description, DateTime _pointBegin, DateTime _pointEnd, Periodicity _period)
+        public Events(int _id)
+        {
+            this.id = _id;
+            this.name = "";
+            this.type = "";
+            this.lesParticipants = new List<Users>();
+            this.description = "";
+            this.pointBegin = new DateTime();
+            this.pointEnd = new DateTime();
+            this.period = 0;
+        }
+        public Events(string _name, string _description, DateTime _pointBegin, DateTime _pointEnd, int _period)
         {
             this.name = _name;
             this.description = _description;
@@ -42,93 +53,5 @@ namespace Agenda.Model
             this.lesParticipants = new List<Users>();
             this.type = "";
         }
-
-
-        //public static Events getEvent(int id)
-        public async Task getEvent(int id)
-        {
-            //using (var webClient = new System.Net.WebClient())
-            //{
-            //    // var json = webClient.DownloadString("http://82.236.45.188/AgendaWS/events/"+id);
-            //    //JObject jObject = JObject.Parse(json);
-            //    //JToken jEvent = jObject;
-            //    //unEvent.name = (string)jEvent["name"];
-            //    //unEvent.description = (string)jEvent["description"];
-            //    //unEvent.type = (string)jEvent["type"];
-            //}
-
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://82.236.45.188/AgendaWS/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage response = await client.GetAsync("events/" + id);
-            if (response.IsSuccessStatusCode)
-            {
-                Events theEvent = await response.Content.ReadAsAsync<Events>();
-                this.id = theEvent.id;
-                this.name = theEvent.name;
-                this.description = theEvent.description;
-            }
-        }
-        public static List<Events> getEvents()
-        {
-            List<Events> lesEvents = new List<Events>();
-
-            using (var webClient = new System.Net.WebClient())
-            {
-                var json = webClient.DownloadString("http://82.236.45.188/AgendaWS/events");
-                JArray arr = JArray.Parse(json);
-
-                for (int i = 0; i < arr.Count; i++)
-                {
-                    JObject jObject = JObject.Parse(arr[i].ToString());
-                    JToken jEvent = jObject;
-                    Events newEvent = new Events();
-                    newEvent.name = (string)jEvent["name"];
-                    newEvent.description = (string)jEvent["description"];
-                    newEvent.type = (string)jEvent["type"];
-                    lesEvents.Add(newEvent);
-                }
-            }
-
-            return lesEvents;
-        }
-
-        public async Task addEvent()
-        {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://82.236.45.188/AgendaWS/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage response = await client.PostAsJsonAsync("events", this);
-        }
-
-        public async Task editEvent()
-        {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://82.236.45.188/AgendaWS/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage response = await client.GetAsync("events" + this.id);
-            if (response.IsSuccessStatusCode)
-            {
-                Uri eventUrl = response.Headers.Location;
-                response = await client.DeleteAsync(eventUrl);
-            }
-        }
-
-        public async Task deleteEvent()
-        {
-            HttpClient client = new HttpClient();
-            client.BaseAddress = new Uri("http://82.236.45.188/AgendaWS/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            HttpResponseMessage response = await client.DeleteAsync("events/" + this.id);
-        }
-
-        public DateTime getPointBegin() { return this.pointBegin; }
-        public DateTime getPointEnd() { return this.pointEnd; }
-        public Periodicity getPeriod() { return this.period; }
-
     }
 }
--- Agenda/Model/Users.cs
+++ Agenda/Model/Users.cs
@@ -11,52 +11,22 @@ namespace Agenda.Model
 {
     class Users
     {
+        public int id { get; set; }
         public string email { get; set; }
         public string password { get; set; }
-        private List<Users> listeUsers;
 
         public Users()
         {
-            this.email = "";
-            this.password = "";
+        }
+        public Users(int _id)
+        {
+            this.id = _id;
         }
         public Users(string _email, string _password)
         {
             this.email = _email;
             this.password = _password;
             this.listeUsers = new List<Users>();
-        }
-
-        public List<Users> getUsers(){
-
-
-            return this.listeUsers;
-        }
-
-        public void createUser()
-        {
-            using (var client = new HttpClient())
-            {
-
-            }
-
-        }
-
-        public static Users getUser()
-        {
-            Users unUser = new Users();
-
-            using (var webClient = new System.Net.WebClient())
-            {
-               // var json = webClient.DownloadString("http://82.236.45.188/AgendaWS/events/1");
-                var json = webClient.DownloadString("http://82.236.45.188/VroumVroum/users/1");
-                JObject jObject = JObject.Parse(json);
-                JToken jUser = jObject;
-                unUser.email = (string)jUser["email"];
-                unUser.password = (string)jUser["userToken"];
-            }
-
-            return unUser;
         }
     }
 }
